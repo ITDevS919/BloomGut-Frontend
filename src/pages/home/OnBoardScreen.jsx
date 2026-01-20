9ee07853d746a9a3d1849b2b65d010710c00799e
ITDevS919 <tanyingying429@gmail.com>
2026-01-20 01:39:46 -0500

    Fix UI

--- src/pages/home/OnBoardScreen.jsx
+++ src/pages/home/OnBoardScreen.jsx
@@ -49,19 +49,19 @@ const OnBoardScreen = () => {
         <img
           src={BloomGut}
           alt="Logo"
-          className=" object-cover"
+          className="object-cover"
         />
       </div>
-      <div className="flex flex-col items-center text-center gap-4 mt-12">
+      <div className="flex flex-col items-center text-center gap-4 mt-10">
         {slides[activeSlide].icon}
-        <h3 className="font-semibold text-lg text-primary leading-2 mt-12">
+        <h3 className="font-semibold text-lg text-primary leading-2 mt-10">
           {slides[activeSlide].title}
         </h3>
         <p className="text-sm leading-[18px] text-primary">
           {slides[activeSlide].description}
         </p>
       </div>
-      <div className="flex gap-2 mt-15">
+      <div className="flex gap-2 mt-10">
         {slides.map((_, index) => (
           <Button
             variant="link"
@@ -75,7 +75,7 @@ const OnBoardScreen = () => {
         <div className="mt-4">
           <Button
             variant="outline"
-            className="border border-custom-16 shadow-sm mt-10"
+            className="border border-custom-16 shadow-sm mt-8"
             onClick={() => navigate("/home")}
           >
             Start Now
