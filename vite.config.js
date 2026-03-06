3d28f36e1b99ee7305aa52026ec678a80dc604be
ITDevS919 <tanyingying429@gmail.com>
2026-03-06 04:42:37 -0500

    Final Version

--- vite.config.js
+++ vite.config.js
@@ -34,8 +34,8 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000,
-    allowedHosts: ["www.bloomgut.app"]
-  }
+    allowedHosts: ["www.bloomgut.app", "bloomgut.app"]
+  },
   build: {
     outDir: "dist"
   },
