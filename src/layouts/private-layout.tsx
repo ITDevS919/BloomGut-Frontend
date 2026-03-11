afb02ffff55b4d776973901865595adb4e5c3570
ITDevS919 <tanyingying429@gmail.com>
2026-03-11 08:27:10 -0400

    Bottom Navbar Fixed

--- src/layouts/private-layout.tsx
+++ src/layouts/private-layout.tsx
@@ -209,7 +209,7 @@ const PrivateLayout = ({ children }) => {
   const [selectedRecordOption, setSelectedRecordOption] = useState("");
   const [selectedTrendOption, setSelectedTrendOption] = useState("");
   return (
-    <div className="flex flex-col h-screen relative overflow-hidden">
+    <div className="flex flex-col h-screen relative">
       <div className="flex-1 overflow-y-auto pb-24 min-h-0">{children}</div>
 
       <div className="flex justify-between items-center w-full px-2 py-4 bg-[#EFEBE4] absolute bottom-0 left-0 z-10 shadow-sm border border-custom-8">
