b4ffd33dc9a49b56819a60b4f8405e2b42939279
ITDevS919 <tanyingying429@gmail.com>
2026-03-20 03:37:54 -0400

    Final Version(Subscription Fixed)

--- src/pages/settings/Plan/Plan.jsx
+++ src/pages/settings/Plan/Plan.jsx
@@ -262,11 +262,11 @@ const Plan = () => {
                 </div>
 
                 {/* Pricing Options */}
-                <div className="flex items-center justify-center gap-3 mt-2">
+                <div className="flex items-center justify-center gap-2 mt-2">
                   <button
                     type="button"
                     onClick={() => setSelectedProPricing("6mo")}
-                    className={`text-sm rounded-[8px] px-4 py-2 shadow-sm ${
+                    className={`text-sm rounded-[8px] px-2 py-2 shadow-sm ${
                       selectedProPricing === "6mo"
                         ? "bg-white border text-primary shadow-[0_4px_12px_rgba(0,0,0,0.08)]"
                         : "text-secondary"
@@ -277,7 +277,7 @@ const Plan = () => {
                   <button
                     type="button"
                     onClick={() => setSelectedProPricing("year")}
-                    className={`text-sm rounded-[8px] px-4 py-2 shadow-sm transition-colors ${
+                    className={`text-sm rounded-[8px] px-2 py-2 shadow-sm transition-colors ${
                       selectedProPricing === "year"
                         ? "bg-white border text-primary shadow-[0_4px_12px_rgba(0,0,0,0.08)]"
                         : "text-secondary"
